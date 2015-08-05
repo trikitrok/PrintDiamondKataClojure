a1f292a92c6d8785c7dad2d3a45d6f1809b8a1d5
trikitrok <trikitrok@gmail.com>
2015-08-05 10:52:35 +0200

    Renamed some helper functions and bindings

--- src/print_diamond/core.clj
+++ src/print_diamond/core.clj
@@ -5,22 +5,20 @@
 
 (def ^:private alphabet (map str (char-range \A \Z)))
 
-(defn whole-letters-line [letter]
+(defn build-line-to-be-repeated [letter]
   (let [letters-until (concat (take-while #(not= % letter) alphabet) [letter])]
     (apply str (concat (reverse (drop 1 letters-until)) letters-until))))
 
-(defn introduce-spaces [line letter]
+(defn substitute-spaces [line letter]
   (apply str (map #(if (= (str %) letter) letter "_") line)))
 
-(defn lines-number-in-tringle [letter]
+(defn lines-number-in-uppper-triangle [letter]
   (inc (.indexOf alphabet letter)))
 
 (defn upper-diamond-lines [letter]
-  (let [line (whole-letters-line letter)
-        num-triangle-lines (lines-number-in-tringle letter)]
-    (map introduce-spaces
-         (repeat num-triangle-lines line)
-         (take num-triangle-lines alphabet))))
+  (let [line (build-line-to-be-repeated letter)
+        times (lines-number-in-uppper-triangle letter)]
+    (map substitute-spaces (repeat times line) (take times alphabet))))
 
 (defn diamond-lines [letter]
   (let [upper-part (upper-diamond-lines letter)]
